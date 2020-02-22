9d5f2d87462100e379ac287efea1f2aab1a69f07
divshekhar <imdshekhar@gmail.com>
2020-02-22 14:05:26 +0530

    eye_detector.py added

---
+++ faceDetection/eye_detector.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+
+eye_cascade = cv.CascadeClassifier('./haarcascade_eye_tree_eyeglasses.xml')
+
+face_cascade = cv.CascadeClassifier('./haarcascade_frontalface_default.xml')
+# Read the input image
+# img = cv.imread('../img/rohit.jpg')
+cap = cv.VideoCapture(0)
+while cap.isOpened():
+    _, img = cap.read()
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv.rectangle(img, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 3)
+
+    # Display the Image
+    cv.imshow('Image', img)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
