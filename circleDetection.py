a207ced7a41809f4675bcf95985157cd7bfaeebf
divshekhar <imdshekhar@gmail.com>
2020-08-22 13:57:38 +0530

    Circle Detection

--- circleDetection.py
+++ circleDetection.py
@@ -8,14 +8,15 @@ def circleDetection():
     (x-xc)^2 + (y-yc)^2 = r^2   
     #Formula of circle
     '''
-    img = cv.imread('./img/balls2.jpg')
-    img = cv.resize(img, (500, 400))
+    img = cv.imread('./img/coins.jpg')
+    img = cv.resize(img, (700, 400))
     output = img.copy()
     gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     blur = cv.medianBlur(gray_img, 5)
-    circles = cv.HoughCircles(gray_img, cv.HOUGH_GRADIENT,
-                              1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
-
+    circles = cv.HoughCircles(blur, cv.HOUGH_GRADIENT,
+                              1, 100, 10, param1=130, param2=55, minRadius=5, maxRadius=0)
+    # circles = cv.HoughCircles(blur, cv.HOUGH_GRADIENT,
+    #                           1, 10)
     detected_circles = np.uint16(np.around(circles))
 
     for (x, y, r) in detected_circles[0, :]:
