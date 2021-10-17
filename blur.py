41217f1acb40001ae178d2014273bffd98f0c104
pragyan270 <92673186+pragyan270@users.noreply.github.com>
2021-10-17 17:43:39 +0530

    Update blur.py

--- blur.py
+++ blur.py
@@ -12,6 +12,7 @@ def blurring():
     kernel = np.ones((5, 5), np.float32)/25
     dst = cv.filter2D(img, -1, kernel)
     blur = cv.blur(img, (5, 5))
+    # gaussian blur
     gblur = cv.GaussianBlur(img, (5, 5), 0)
     median = cv.medianBlur(img, 5)
     bilateralFilter = cv.bilateralFilter(img, 9, 75, 75)
